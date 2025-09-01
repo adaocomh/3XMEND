a8c13cbd37a7a270f041f2c4a458dd70fe97f213
Adão com H <adaocomh@gmail.com>
2025-09-01 12:34:22 -0300

    Redução de codigo no componente "VideoCard".

--- src/app/components/videoCard/index.tsx
+++ src/app/components/videoCard/index.tsx
@@ -19,8 +19,7 @@ export default function VideoCards({ src, poster }: VideoCardsProps){
     }
 
     return(
-        <div className='w-[100%] h-[100%] bg-cover rounded-[10px] mb-[10px] overflow-hidden cursor-pointer' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <video ref={videoRef} className='w-full h-full object-cover' muted loop preload="auto" poster={poster} onLoadedData={() => {
+            <video ref={videoRef} className='w-[100%] h-[100%] rounded-[10px] mb-[10px] cursor-pointer' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} muted loop preload="auto" poster={poster} onLoadedData={() => {
                 if(videoRef.current){
                     videoRef.current.currentTime = 0.1
                 }
@@ -28,6 +27,5 @@ export default function VideoCards({ src, poster }: VideoCardsProps){
             <source src={src} type="video/mp4"/>
             Seu navegador não suporta vídeos HTML5.
             </video>
-        </div>
     )
 }
