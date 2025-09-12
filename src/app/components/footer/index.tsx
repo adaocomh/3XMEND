a8e0be057975791173dd4f5342c8f80aefe40b32
Adão com H <adaocomh@gmail.com>
2025-09-12 09:35:35 -0300

    Footer refatorado.

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,11 +1,10 @@
+import AlternadorIdiomaFooter from "../alternadorFooter"
 import BtnAgencia from "../btnContatoFooter"
-import Image from "next/image"
 import Slide from "../elementosSlides"
 import FadeIn from "../elementosFadeIn"
 import Line from "../linhaCrescente"
 import {useTranslations} from 'next-intl';
 import LinksContato from "../linksContatos"
-import AlternadorIdiomaFooter from "../alternadorFooter"
 
 export default function Footer(){
     const t = useTranslations('footer');
@@ -13,10 +12,10 @@ export default function Footer(){
 
     return(
         <div className="flex flex-col justify-center items-center w-[100vw] py-[50px] bg-[var(--cor-secundaria)]">
-            <div className="flex flex-col justify-center gap-[60px] items-center w-[95%] md:w-[1200px] mb-[50]">
+            <div className="flex flex-col justify-center items-center gap-[60px] w-[95%] mb-[50] md:w-[1200px]">
                 <div className="overflow-hidden">
                     <Slide>
-                        <h1 className="text-center text-[80px] md:text-[100px] text-(--background) font-bold">{t('titleFooter')}</h1>
+                        <h1 className=" text-[80px] text-center font-bold text-(--background) md:text-[100px]">{t('titleFooter')}</h1>
                     </Slide>
                 </div>
                 <div className="overflow-hidden">
@@ -27,14 +26,14 @@ export default function Footer(){
                 <div className="w-full">
                 <Line/>
                 </div>
-                <div className="flex flex-col w-full gap-[50px]">
+                <div className="flex flex-col gap-[50px] w-full">
                     <FadeIn>
                     <div className="flex flex-col overflow-hidden">
-                        <h6 className="text-[16px] text-(--background) text-center font-bold">NovAmerica Office Park</h6>
-                        <p className="fade-in text-[16px] text-(--background) text-center font-light h-[50px]">Av. das Nações Unidas, 18801 - Santo Amaro, São Paulo - SP, 04753-100, Brasil</p>
+                        <h6 className="text-[16px] font-bold text-center text-(--background)">NovAmerica Office Park</h6>
+                        <p className="h-[50px] text-[16px] font-light text-center text-(--background) fade-in">Av. das Nações Unidas, 18801 - Santo Amaro, São Paulo - SP, 04753-100, Brasil</p>
                     </div>
-                    <div className=" flex flex-col gap-[40px] md:flex-row justify-between items-center w-full">
-                        <p className="order-3 md:order-0 text-[16px] text-(--foreground)) text-center text-(--foreground) font-light  w-[350px]">{t('copy')}</p>
+                    <div className="flex flex-col justify-between items-center gap-[40px] w-full md:flex-row">
+                        <p className="order-3  w-[350px] text-[16px] font-light text-center text-(--foreground) md:order-0">{t('copy')}</p>
                         <LinksContato/>
                         <AlternadorIdiomaFooter/>
                     </div>
